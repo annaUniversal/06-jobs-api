2cf77ce3ae985edc5735b608525c971db8ec710d
annaUniversal <anna.universal@yahoo.com>
2023-12-08 11:52:31 -0500

    Lost Changes

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,24 +1,30 @@
+const Task = require("../models/Task");
+const { StatusCodes } = require("http-status-codes");
+const { BadRequestError, NotFoundError } = require("../errors");
+
 const getAllTasks = async (req, res) => {
-    res.send('Get All Tasks')
-}
+  res.send("Get All Tasks");
+};
 
 const getTask = async (req, res) => {
-    res.send('Get a Task')
-}
+  res.send("Get a Task");
+};
 const createTask = async (req, res) => {
-    res.send('Create a Task')
-}
+  req.body.createdBy = req.user.userId;
+  const task = await Task.create(req.body);
+  res.status(StatusCodes.CREATED).json({task});
+};
 const deleteTask = async (req, res) => {
-    res.send('Delete the Task')
-}
+  res.send("Delete the Task");
+};
 const updateTask = async (req, res) => {
-    res.send('Update the Task')
-}
+  res.send("Update the Task");
+};
 
 module.exports = {
-    getAllTasks,
-    getTask,
-    createTask,
-    updateTask,
-    deleteTask,
-}
+  getAllTasks,
+  getTask,
+  createTask,
+  updateTask,
+  deleteTask,
+};
